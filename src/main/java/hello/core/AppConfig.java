8726814332589848207b351b75781625572445a8
sonjarvis <sonjarvis96@gmail.com>
2025-01-18 14:26:03 +0900

    프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,22 +1,34 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+//구성영역
 public class AppConfig {
 
+    //생성자 주입 : 생성자를 통해서 객체가 유인스턴스 생성된게 들어감
+    //멤버 서비스 인플을 만들고 내가 만든 멤버 서비스 인플은 메모리 멤버 리포지토리를 사용할 거야  --> 생성자 주입
     public MemberService memberService(){
-        //생성자 주입 : 생성자를 통해서 객체가 유인스턴스 생성된게 들어감
-        //멤버 서비스 인플을 만들고 내가 만든 멤버 서비스 인플은 메모리 멤버 리포지토리를 사용할 거야  --> 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository()); //구현체 객
+        return new MemberServiceImpl(memberRepository()); //구현체 객
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy()); //생성자 주입
+        return new OrderServiceImpl(memberRepository(), discountPolicy()); //생성자 주입
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy(); //원하는 할인 정책으로 변경만 하면 끝
     }
 
 }
