7a20fd40c0d36b9e1b28b3f22e7b520b4ad702db
sonjarvis <sonjarvis96@gmail.com>
2025-01-18 17:07:26 +0900

    스프링 컨테이너 구성 및 스프링 빈

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //구성영역
+@Configuration
 public class AppConfig {
 
     //생성자 주입 : 생성자를 통해서 객체가 유인스턴스 생성된게 들어감
     //멤버 서비스 인플을 만들고 내가 만든 멤버 서비스 인플은 메모리 멤버 리포지토리를 사용할 거야  --> 생성자 주입
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); //구현체 객
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy()); //생성자 주입
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy(); //원하는 할인 정책으로 변경만 하면 끝
     }
